fd78cde37bd421164c65fc30b8a22625c0fe73f4
Lauren March <laurenbmarch@gmail.com>
2021-10-02 23:28:58 -0400

    after save 10-2, 11:28 pm

--- ChallengeFour_Console/ProgramUI.cs
+++ ChallengeFour_Console/ProgramUI.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ChallengeFour_RepositoryA;
+
 
 namespace ChallengeFour_Console
 {
-    class Class1
+    public class ProgramUI
     {
+        private OutingRepo _outingRepo = new OutingRepo();
+        public void Run()
+        {
+        //Menu
+        
+        }
     }
 }
--- ChallengeFour_RepositoryA/OutingRepo.cs
+++ ChallengeFour_RepositoryA/OutingRepo.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ChallengeFour_RepositoryA
 {
-    class OutingRepo
+    public class OutingRepo
     {
         private List<Outing> _listOfOutings = new List<Outing>();
         
@@ -15,27 +15,37 @@ namespace ChallengeFour_RepositoryA
         {
             return _listOfOutings;
         }
+       
         //Add Outing to List
         public void AddOutingToList(Outing newOuting)
         {
             _listOfOutings.Add(newOuting);
         }
+        
         //View Combined Costs of All Outings
-        //public decimal AddAllOutingCosts()
-        //{
-        //    foreach(Outing outing in _listOfOutings)
-        //    {
-        //        decimal totalCostOfEvent = outing.TotalCostForEvent;
-                
-        //    }
-        //}
+        public decimal AddAllOutingCosts()
+        {
+            decimal totalOutingCosts = _listOfOutings.Sum(outing => outing.TotalCostForEvent);
+            return totalOutingCosts;
+        }
+        
         //View Costs of Outings By Type
-        //public decimal AddCostsByType(EventType typeOfEvent)
-        //{
-        //    Outing outingByType = GetOutingByType(typeOfEvent);
-            
-        //}
-        //Helper Method
+        public decimal AddCostsByType(EventType typeOfEvent)
+        {
+            List<Outing> listByType = new List<Outing>();
+            foreach (Outing outing in _listOfOutings)
+            {
+                if(typeOfEvent == outing.TypeOfEvent)
+                {
+                    listByType.Add(outing);
+                }
+            }
+
+            decimal SumByEventType = listByType.Sum(outing => outing.TotalCostForEvent);
+            return SumByEventType;
+        }
+        
+        //Helper Method -- Question: Not sure if I need this
         public Outing GetOutingByType(EventType typeOfEvent)
         {
             foreach(Outing outing in _listOfOutings)
